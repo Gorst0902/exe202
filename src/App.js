32da09eda8edd5e536bdc111e32ccc878f90cd5a
Gorst0902 <dinhvanthanhan0902@gmail.com>
2023-10-05 22:56:32 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,9 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { AuthProvider } from "./context/AuthContext";
 import Footer from "./pages/Footer/Footer";
 import Reservation from "./pages/Reservation/Reservation";
-<<<<<<< HEAD
 import ReservationDetail from "./pages/ReservationDetail/ReservationDetail";
-=======
 import AddressAutocomplete from "./pages/Booking/LocationService";
 import AddressForm from "./pages/Booking/DeliveryForm";
->>>>>>> 6d528e1e199978de9e2a00a7263c97b0685bb2a7
 
 function App() {
   return (
@@ -27,12 +24,9 @@ function App() {
             <Route path="/signUp" element={<SignUp />} />
             <Route path="/footer" element={<Footer />} />
             <Route path="/driver" element={<Reservation />} />
-<<<<<<< HEAD
             <Route path="/reservationDetail/:reservationId" element={<ReservationDetail />} />
-=======
             <Route path="/user" element={<AddressAutocomplete />} />
             <Route path="/book" element={<AddressForm />} />
->>>>>>> 6d528e1e199978de9e2a00a7263c97b0685bb2a7
           </Routes>
         </Router>
       </AuthProvider>

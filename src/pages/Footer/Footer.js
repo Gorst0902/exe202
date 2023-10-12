1e8491e3ea7195f1eec153c44d6d971d38010a55
hung-nv95 <hungnvse151468@fpt.edu.vn>
2023-10-13 00:31:36 +0700

    update dashboard

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -5,21 +5,11 @@ export default function Footer() {
   return (
     <div className="footer">
       <div className="footer__content">
-        <button>
-          <i className="fa-solid fa-house" />
-        </button>
-        <button>
-          <i className="fa-solid fa-list" />
-        </button>
-        <button className="search">
-          <i className="fa-solid fa-magnifying-glass" />
-        </button>
-        <button>
-          <i className="fa-solid fa-bell" />
-        </button>
-        <button>
-          <i className="fa-solid fa-gear" />
-        </button>
+        <button className="fa fa-home" />
+        <button className="fa fa-list" />
+        <button className="fa fa-search search" />
+        <button className="fa fa-bell" />
+        <button className="fa fa-gear" />
       </div>
     </div>
   );

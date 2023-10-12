2c81fe2c68278d7456cd6dce6f94875fbb8038d1
hung-nv95 <hungnvse151468@fpt.edu.vn>
2023-10-13 00:57:48 +0700

    test footer

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -5,11 +5,21 @@ export default function Footer() {
   return (
     <div className="footer">
       <div className="footer__content">
-        <button className="fa fa-home" />
-        <button className="fa fa-list" />
-        <button className="fa fa-search search" />
-        <button className="fa fa-bell" />
-        <button className="fa fa-gear" />
+        <button className="fa fa-home">
+          <i className="fa fa-home" />
+        </button>
+        <button className="fa fa-list">
+          <i className="fa fa-list" />
+        </button>
+        <button className="fa fa-search search">
+          <i className="fa fa-search" />
+        </button>
+        <button className="fa fa-bell">
+          <i className="fa fa-bell" />
+        </button>
+        <button className="fa fa-gear">
+          <i className="fa fa-gear" />
+        </button>
       </div>
     </div>
   );
